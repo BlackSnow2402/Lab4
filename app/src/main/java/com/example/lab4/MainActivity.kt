cdbe0a197f593b9adfe69c1959959b4136e23045
Ульященко Михаил Юрьевич <mihail@soft-masters.biz>
2023-06-08 19:15:14 +0700

    Prev button, arrows and UI

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -29,12 +31,22 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View -> checkAnswer(true) }
         falseButton.setOnClickListener { view: View -> checkAnswer(false) }
 
-        nextButton.setOnClickListener { currentIndex = (currentIndex + 1) % questionBank.size
+        questionTextView.setOnClickListener { currentIndex =
+            (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        nextButton.setOnClickListener { currentIndex =
+            (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener { currentIndex =
+            (currentIndex + questionBank.size - 1) % questionBank.size
             updateQuestion()
         }
         updateQuestion()
