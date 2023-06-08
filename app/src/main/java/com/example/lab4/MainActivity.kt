47d250d9ebd3386950bc927bad9d57c5d30e8d91
Ульященко Михаил Юрьевич <mihail@soft-masters.biz>
2023-06-08 22:58:39 +0700

    Saving of current question (QuizViewModel)

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -15,17 +19,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
-    private var currentIndex = 0
+    private val quizViewModel: QuizViewModel by
+    lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -37,27 +39,49 @@ class MainActivity : AppCompatActivity() {
         trueButton.setOnClickListener { view: View -> checkAnswer(true) }
         falseButton.setOnClickListener { view: View -> checkAnswer(false) }
 
-        questionTextView.setOnClickListener { currentIndex =
-            (currentIndex + 1) % questionBank.size
+        questionTextView.setOnClickListener {
+            quizViewModel.moveToNext()
             updateQuestion()
         }
-        nextButton.setOnClickListener { currentIndex =
-            (currentIndex + 1) % questionBank.size
+        nextButton.setOnClickListener {
+            quizViewModel.moveToNext()
             updateQuestion()
         }
-        prevButton.setOnClickListener { currentIndex =
-            (currentIndex + questionBank.size - 1) % questionBank.size
+        prevButton.setOnClickListener {
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
         updateQuestion()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId =
             if (userAnswer == correctAnswer) {
                 R.string.correct_toast
---
+++ app/src/main/java/com/example/lab4/QuizViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.lab4
+
+import androidx.lifecycle.ViewModel
+
+class QuizViewModel : ViewModel() {
+    var currentIndex = 0
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    val currentQuestionAnswer: Boolean get() = questionBank[currentIndex].answer
+    val currentQuestionText: Int get() = questionBank[currentIndex].textResId
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+    fun moveToPrev() {
+        currentIndex = (currentIndex + questionBank.size - 1) % questionBank.size
+    }
+}
